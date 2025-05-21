93c339fc16153d8e447f4aaa4758d89dd6c54621
masoudgoldx <masoud.msv541@gmail.com>
2025-05-22 02:05:49 +0330

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,12 +1,33 @@
 
 from news_engine import get_and_analyze_news
 from tech_analysis import get_technical_analysis
+from internal_news import get_internal_news
 from utils import send_telegram_message, load_last_message, save_last_message
 
 def main():
-    news_text = get_and_analyze_news()
-    tech_text = get_technical_analysis()
-    final_message = news_text + "\n\n" + tech_text
+    internal = get_internal_news()
+    international = get_and_analyze_news()
+    technical = get_technical_analysis()
+
+    final_message = (
+        "اخبار داخلی و اثرگذاری بر بازار:
+
+" + internal + 
+        "
+
+--------------------------
+
+" +
+        "اخبار بین‌المللی مهم:
+
+" + international +
+        "
+
+--------------------------
+
+" +
+        technical
+    )
 
     if final_message.strip() != load_last_message():
         send_telegram_message(final_message)

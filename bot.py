dddff814ac5d47fc671c5657b3673e259d8e98aa
masoudgoldx <masoud.msv541@gmail.com>
2025-05-21 15:37:17 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,34 +1,14 @@
 import requests
-from tech_analysis import get_analysis
-
-def get_news():
-    # این قسمت رو بعداً با خبر واقعی جایگزین می‌کنی
-    fundamental = "خبر فاندامنتال جدید"  # این خط تست است
-    analysis = get_analysis()  # خروجی تحلیل تکنیکال
-    return f"{fundamental}\n{analysis}"
-
-def read_last_news():
-    try:
-        with open('last_news_id.txt', 'r') as f:
-            return f.read().strip()
-    except FileNotFoundError:
-        return ""
-
-def write_last_news(news):
-    with open('last_news_id.txt', 'w') as f:
-        f.write(news)
 
 def send_telegram_message(message):
-    url = "https://api.telegram.org/bot<توکن_ربات>/sendMessage"
+    url = "https://api.telegram.org/bot7352244492:AAGOrkQXT88z1OH975q09jWkBcoI3G3ifEQ/sendMessage"
     payload = {
-        "chat_id": "<chat_id>",
+        "chat_id": "-1002586854094",
+        "message_thread_id": 2,
         "text": message
     }
     requests.post(url, data=payload)
 
 if name == "__main__":
-    news = get_news()
-    last_news = read_last_news()
-    if news != last_news and news.strip() != "":
-        send_telegram_message(news)
-        write_last_news(news)
+    message = "تست ارسال پیام با MasoudGOLDXBot (غیرتکراری)"
+    send_telegram_message(message)

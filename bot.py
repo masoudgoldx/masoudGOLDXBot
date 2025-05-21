bcf90ae20d645542ef13d410e8bb7a85e4209fb9
masoudgoldx <masoud.msv541@gmail.com>
2025-05-21 08:21:30 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,8 +3,8 @@ import requests
 def main():
     url = "https://api.telegram.org/bot7352244492:AAGOrkQXT88z1OH975q09jWkBcoI3G3ifEQ/sendMessage"
     payload = {
-        "chat_id": "-1002586854094",   # آیدی گروه با منفی
-        "message_thread_id": 2,         # آیدی تاپیک (در صورت نیاز تغییر بده)
+        "chat_id": "-1002586854094",
+        "message_thread_id": 2,
         "text": "✅ تست موفق! MasoudGoldXBot فعال شد و آماده ارسال اخبار فاندامنتال است."
     }
     requests.post(url, data=payload)

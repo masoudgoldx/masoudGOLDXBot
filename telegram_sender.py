c58a74a35ddd0c9ce034d64c7d35deb2ec1f7d22
masoudgoldx <masoud.msv541@gmail.com>
2025-05-24 20:28:26 +0330

    Add files via upload

--- telegram_sender.py
+++ telegram_sender.py
@@ -5,15 +5,10 @@ import requests
 def send_telegram_message(message):
     token = os.getenv("BOT_TOKEN")
     chat_id = os.getenv("CHAT_ID")
-
     if not token or not chat_id:
-        print("توکن یا chat_id تنظیم نشده.")
+        print("BOT_TOKEN or CHAT_ID missing")
         return
-
     url = f"https://api.telegram.org/bot{token}/sendMessage"
-    max_len = 4000
-
-    for i in range(0, len(message), max_len):
-        part = message[i:i+max_len]
-        response = requests.post(url, data={"chat_id": chat_id, "text": part})
-        print(f"Status: {response.status_code}, Response: {response.text}")
+    for i in range(0, len(message), 4000):
+        part = message[i:i+4000]
+        requests.post(url, data={"chat_id": chat_id, "text": part})

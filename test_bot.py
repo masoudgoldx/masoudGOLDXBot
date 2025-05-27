ff257bc7c4bf5c9ee7fdbf3f28f151f518959e14
masoudgoldx <masoud.msv541@gmail.com>
2025-05-27 22:15:24 +0330

    Update test_bot.py

--- test_bot.py
+++ test_bot.py
@@ -1,10 +1,3 @@
-import requests
+from telegram_sender import send_telegram_message
 
-url = "https://api.telegram.org/bot7352244492:AAGOrkQXT88z1OH975q09jWkBcoI3G3ifEQ/sendMessage"
-payload = {
-    "chat_id": "-1002586854094",
-    "text": "فقط تست خام MasoudGOLDXBot"
-}
-r = requests.post(url, data=payload)
-print(r.status_code)
-print(r.text)
+send_telegram_message("✅ تست موفق بود! MasoudGOLDX آماده است.")

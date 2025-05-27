f55831975db896bb3fa983104379e1efcc7991b6
masoudgoldx <masoud.msv541@gmail.com>
2025-05-27 15:59:33 +0330

    Update telegram_sender.py

--- telegram_sender.py
+++ telegram_sender.py
@@ -8,6 +8,8 @@ def send_telegram_message(text):
         "text": text,
         "parse_mode": "HTML"
     }
+
     response = requests.post(url, data=data)
     if response.status_code != 200:
-        print("خطا در ارسال پیام:", response.text)
+        print("❌ خطا در ارسال پیام:", response.status_code)
+        print("پاسخ سرور:", response.text)

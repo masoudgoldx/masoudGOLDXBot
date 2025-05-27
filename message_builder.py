a96d2b18c02f8adace8db8736c2667e41f1b0fbf
masoudgoldx <masoud.msv541@gmail.com>
2025-05-27 15:43:47 +0330

    Update message_builder.py

--- message_builder.py
+++ message_builder.py
@@ -1,16 +1,9 @@
-
-def build_message(news_by_asset, tech, local, calendar):
-    message = "📢 گزارش روزانه Masoudgoldx\n\n"
-
-    for asset, items in news_by_asset.items():
-        if items:
-            message += f"== {asset} ==\n"
-            for item in items:
-                message += f"{item['title']}\n{item['summary']}\n{item['impact']}\n{item['link']}\n\n"
-
-    message += tech + "\n\n"
-    message += local + "\n\n"
-    message += calendar + "\n\n"
-    message += "تحلیل و گردآوری: Masoudgoldx"
-    return message
-return f"{msg}\n\n🆕 ست دوم"
+def build_message(title, data_list):
+    """
+    ساخت پیام نهایی برای ارسال به تلگرام
+    """
+    header = f"<b>{title}</b>\n"
+    body = ""
+    for item in data_list:
+        body += f"{item}\n"
+    return header + body
